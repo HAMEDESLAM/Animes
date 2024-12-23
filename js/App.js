e75864032644ab24d531b4982390b4e06d79c43b
MR.DB <486196148@giza1.moe.edu.eg>
2024-12-23 20:56:35 +0200

    After JS

---
+++ js/App.js
@@ -0,0 +1,22 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+let totop = document.getElementById("totop")
+window.onscroll = function() {
+    const landingHeight = document.querySelector('.landing').offsetHeight;
+    if(window.scrollY >= landingHeight){
+        totop.style.opacity = "1";
+        totop.style.zIndex = "100000000";
+        totop.style.position = "static";
+    }
+    else{
+        totop.style.opacity = "0";
+        totop.style.zIndex = "-1";
+        totop.style.position = "absolute";
+    }
+};
+
+let btn = document.getElementsByClassName("carousel-control-next")[0];
+let handle = setInterval(() => {
+    btn.click();  
+},15000)
